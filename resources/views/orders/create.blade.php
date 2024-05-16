d297087ede08b7ffc469dea09b641ade87d2f8f3
yelanisha <shabayeva05@mail.ru>
2024-05-16 17:15:17 +0400

    111

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -3,29 +3,31 @@
 @section('title', 'Создание товара')
 
 @section('content')
-<section class="comments">
-    <div class="contacts">
-        <img src="{{ asset('css/img/blest.png') }}" alt="" class="contacts_glitter">
-        <span class="contacts_text">Just Create</span>
-        <img src="{{ asset('css/img/ar2.png') }}" alt="" class="cont_arr">
-    </div>
-    <div class="support">
-        <img src="{{ asset('css/img/ar3.png') }}" alt="" class="supp_arr">
-        <div >
-            <p class="title">Create <span class="text_color">Order</span></p>
-            <form action="{{ route('orders.store') }}" class="support_info" method="post">
-                @csrf
-                <input class="comm_input" placeholder="Name" name="name">
-                <input class="comm_input" placeholder="Comment" name="comment">
-                <input hidden class="comm_input" name="user_id" value="1">
-                <select class="form-select" aria-label="Default select example" name="status_id">
-                    @foreach ($statuses as $key => $value)
-                        <option value="{{ $key }}">{{ $value }}</option>
-                    @endforeach
-                </select>
-                <button type="submit" class="support_butt">Send</button>
-            </form>
+    <section class="comments">
+        <div class="contacts">
+            <img src="{{ asset('css/img/blest.png') }}" alt="" class="contacts_glitter">
+            <span class="contacts_text">Just Create</span>
+            <img src="{{ asset('css/img/ar2.png') }}" alt="" class="cont_arr">
         </div>
-    </div>
-</section>
+        <div class="support">
+            <img src="{{ asset('css/img/ar3.png') }}" alt="" class="supp_arr">
+            <div>
+                <p class="title">Create <span class="text_color">Order</span></p>
+                <form action="{{ route('orders.store') }}" class="support_info" method="post">
+                    @csrf
+                    <input class="comm_input" placeholder="Name" name="name">
+                    <input class="comm_input" placeholder="Comment" name="comment">
+                    <input hidden class="comm_input" name="user_id" value="1">
+                    <select class="form-select comm_input"
+                        style="border-radius: 17px;border: 2px solid #6C5FBC;font-weight: 600;font-size: 17px;line-height: 24px;color: #6e6e6e;padding-left: 21px;"
+                        aria-label="Default select example" name="status_id">
+                        @foreach ($statuses as $key => $value)
+                            <option value="{{ $key }}">{{ $value }}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit" class="support_butt">Send</button>
+                </form>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -3,26 +3,34 @@
 @section('title', 'Редактирование товара')
 
 @section('content')
-<div class='container-sm justfify-content-center mt-2'>
-    <form action="{{ route('orders.update', $order->id) }}" method="POST">
-        @CSRF
-        @method('PUT')
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Comment</label>
-            <input class="form-control" name="comment" value="{{ $order->comment }}">
-            <input hidden value="1" name="user_id">
+    <section class="comments">
+        <div class="contacts">
+            <img src="{{ asset('css/img/blest.png') }}" alt="" class="contacts_glitter">
+            <span class="contacts_text">Just Edit</span>
+            <img src="{{ asset('css/img/ar2.png') }}" alt="" class="cont_arr">
         </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Status</label>
-            <select class="form-select" aria-label="Default select example" name="status_id">
-                @foreach ($statuses as $key => $value)
-                    <option {{ $key == $order->status_id ? 'selected' : '' }} value="{{ $key }}">{{ $value }}</option>
-                @endforeach
-            </select>
+        <div class="support">
+            <img src="{{ asset('css/img/ar3.png') }}" alt="" class="supp_arr">
+            <div>
+                <p class="title">Edit <span class="text_color">Order</span></p>
+                <form action="{{ route('orders.update', $order->id) }}" class="support_info" method="post">
+                    @csrf
+                    @method('PUT')
+                    <input class="comm_input" placeholder="Comment" name="comment" value="{{ $order->comment }}">
+                    <input hidden value="1" name="user_id">
+                    <select class="form-select comm_input"
+                        style="border-radius: 17px;border: 2px solid #6C5FBC;font-weight: 600;font-size: 17px;line-height: 24px;color: #6e6e6e;padding-left: 21px;"
+                        aria-label="Default select example" name="status_id">
+                        @foreach ($statuses as $key => $value)
+                            <option {{ $key == $order->status_id ? 'selected' : '' }} value="{{ $key }}">
+                                {{ $value }}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit" class="support_butt">Send</button>
+                </form>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+    </section>
 @endsection
 
 <?php
